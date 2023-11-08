0041b767d75ca7a57f03c33ce1281d423dc79fa3
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:07:13 -0500

    coment

--- Diccionarios y funciones/ejercicioDiccionario/ejercicio 3.py
+++ Diccionarios y funciones/ejercicioDiccionario/ejercicio 3.py
@@ -3,7 +3,7 @@ calificaciones1 = {
  'nombre': 'Sandra', 
  'notafinal': 5.0
  }
-calificaciones = {
+calificaciones2 = {
  'Sandra': 5.0, 
  'Adriana':5.0,
  'Mauricio':4.5,
@@ -11,6 +11,6 @@ calificaciones = {
  }
 
 #items() es un metodo que se utiliza para obtener una vista de pares clave-valor en un diccionario
-for i, j in calificaciones.items():#se utiliza para iterar a traves de las claves y valores en el diccionario "calificaciones"
+for i, j in calificaciones2.items():#se utiliza para iterar a traves de las claves y valores en el diccionario calificaciones2
 
     print(i,j)#imprime los valores de i que es las claves y j que seria los valores en este caso las calificaciones
