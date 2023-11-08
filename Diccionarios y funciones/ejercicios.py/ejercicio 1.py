ed43e6f1d28ba7fbeecfd3bd2a4d596231fdf20b
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:23:45 -0500

    coment

--- Diccionarios y funciones/ejercicios.py/ejercicio 1.py
+++ Diccionarios y funciones/ejercicios.py/ejercicio 1.py
@@ -1,4 +1,4 @@
-#Escriba una función en Python que reproduzca lo siguiente:𝑓(𝑥, 𝑦) = 𝑥2 + 𝑦2 Valor para X=3 y valor para Y=5
+#Escriba una funcion en Python que reproduzca lo siguiente:𝑓(𝑥, 𝑦) = 𝑥2 + 𝑦2 Valor para X=3 y valor para Y=5
 #se define la funcion como calcular y toma dos argumentos que 'x' ,'y'
 def calcular(x, y):
     # Calcula el resultado y lo retorna
@@ -8,7 +8,7 @@ def calcular(x, y):
 x = 3
 y = 5
 
-# Se llama a la función "calcular" con los valores "x" e "y" y se guarda ese resultado en "result"
+# Se llama a la funcion "calcular" con los valores "x" e "y" y se guarda ese resultado en "result"
 result = calcular(x, y)
 
 # Se imprime el resultado
