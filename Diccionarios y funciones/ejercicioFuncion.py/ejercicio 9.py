e52dc3bb72c28b807b6d6412600ccb1bca44116d
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:22:27 -0500

    coment

--- Diccionarios y funciones/ejercicioFuncion.py/ejercicio 9.py
+++ Diccionarios y funciones/ejercicioFuncion.py/ejercicio 9.py
@@ -1,11 +1,11 @@
 #Modificando parámetros mutables
 #se define una funcion llamada 'listalimpia' que tiene dos argumentos:'arg' y 'result con un valor predeterminado de None
-def listalimpia(arg, result=None):
+def listaLimpia(arg, result=None):
     if result is None:#si el argumento 'result' es None 
         result = []#se crea una lista vacia
         result.append(arg)#se agrega el valor del argumento 'arg' a la lista utilizando el metodo .append()
         print(result)
 #llama a la funcion 'listalimpia' con el valor 'a'       
-listalimpia("a")
+listaLimpia("a")
 #llama a la funcion 'listalimpia' con el valor 'b'
-listalimpia("b") 
+listaLimpia("b") 
