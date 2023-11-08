8c31b7217eef49446d26de6aa6f2be47b1e1ae23
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:08:58 -0500

    coment

--- Diccionarios y funciones/ejercicioDiccionario/ejercicio 6.py
+++ Diccionarios y funciones/ejercicioDiccionario/ejercicio 6.py
@@ -1,6 +1,6 @@
 #se definen 2 listas con valores que son una de nombres y otra de edades
-nombres = ['Maria', 'Sebastian', 'Ana']
-edades = ['18', '25', '30']
+nombres = ['Melissa', 'Vanesa', 'Stiven']
+edades = ['16', '17', '18']
 #la funcion zip se utiliza para combinar las dos listas en parejas de elementos
 for n, e in zip(nombres, edades):#for recorre y por cada iteracion n toma el de un nombre y e toma el valor de una edad
 #la funcion format{} Permite combinar valores o variables con una cadena de formato 
