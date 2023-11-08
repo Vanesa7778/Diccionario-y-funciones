51e7985809d970173bd4a6b9edb7a274efadf89e
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:03:33 -0500

    ejercicio 4.py

--- Diccionarios y funciones/ejercicioDiccionario/ejercicio 4.py
+++ Diccionarios y funciones/ejercicioDiccionario/ejercicio 4.py
@@ -5,7 +5,7 @@ calificaciones = {
  'Jose':2.5
  }
 
-print("Técnicas por clave")#se imprime Tecnicas por clave
+print("Tecnicas por clave")#se imprime Tecnicas por clave
 # keys() es un método que se utiliza para obtener una vista de las claves (keys) del diccionario.
 for i in calificaciones.keys():#for recorre cada clave (key) del diccionario, en cada iteracion i toma el valor de una de estas claves en el diccionario
     print(i)#imprime el valor de i que es la clave de calificaciones
