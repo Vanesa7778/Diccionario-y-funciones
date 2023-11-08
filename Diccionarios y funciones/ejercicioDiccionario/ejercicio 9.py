29df0c1aed2679ee75e4b468a87f88ef94d433e3
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:14:20 -0500

    coment

--- Diccionarios y funciones/ejercicioDiccionario/ejercicio 9.py
+++ Diccionarios y funciones/ejercicioDiccionario/ejercicio 9.py
@@ -1,4 +1,4 @@
-calificaciones = {
+calificaciones2 = {
  'Sandra': 5.0, 
  'Adriana': 5.0,
  'Mauricio': 4.5,
@@ -9,7 +9,7 @@ calificaciones = {
 
 #BORRAR UN ELEMENTO DEL DICCIONARIO
 #el del se utiliza para eliminar elementos o variables de listas o diccionarios 
-del(calificaciones['Rosa'])#En este caso del esta elimando la clave rosa con su valor del diccionario elementos
-for i, j in calificaciones.items():#for recoorre y por cada iteracion i toma una clave y j un valor
+del(calificaciones2['Rosa'])#En este caso del esta elimando la clave rosa con su valor del diccionario elementos
+for i, j in calificaciones2.items():#for recoorre y por cada iteracion i toma una clave y j un valor
     print(i,j)#imprime el valor actual de i que son las claves y de j que son los valores
 
