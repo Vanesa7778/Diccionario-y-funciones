330af551fae4299aef83bc54f35ddff9769d3930
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:11:56 -0500

    coment

--- Diccionarios y funciones/ejercicioDiccionario/ejercicio 8.py
+++ Diccionarios y funciones/ejercicioDiccionario/ejercicio 8.py
@@ -1,5 +1,5 @@
-#IMPRIMIR NÚMEROS EN REVERSA
-print("Números en reversa")
+#IMPRIMIR NUMEROS EN REVERSA
+print("Numeros en reversa")
 #la funcion reversed() se utiliza cuando se necesita recorrer una secuencia en orden inverso
 for i in reversed(range(1, 10, 2)):#el for recorre y su secuencia empieza desde 9 hasta 1 donde avanza en 2
     print(i)#se imprime el valor de i que en este caso seria los numeros del 1 al 9 en dos 
