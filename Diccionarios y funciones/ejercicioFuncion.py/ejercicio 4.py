3d2d12670339cef201b143bd1548ec46ebfdd31f
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-11-08 17:19:14 -0500

    coment

--- Diccionarios y funciones/ejercicioFuncion.py/ejercicio 4.py
+++ Diccionarios y funciones/ejercicioFuncion.py/ejercicio 4.py
@@ -1,9 +1,9 @@
-#se define una funcion llamada 'validarsiexiste' que toma un argumento 'obj'
-def validarsiexiste(obj):
+#se define una funcion llamada 'validarSiExiste' que toma un argumento 'obj'
+def validarSiExiste(obj):
     #comprueba si obj es verdadero o falso
     if obj:#en caso de que sea verdadero imprime un mensaje is true
         print(f"{obj} is True")#dato booleano
     else:#en caso de que sea falso imprime un mensaje is false
         print(f"{obj} is False")#dato booleano
 
-validarsiexiste(1)#validando la exitencia de un objeto 
+validarSiExiste(1)#validando la exitencia de un objeto 
